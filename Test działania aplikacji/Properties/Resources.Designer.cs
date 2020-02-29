fd9aa727da53f1a9a098dd92ebd53333cb410d2e
Jakub Sulej <sulejmedia@gmail.com>
2020-02-29 14:52:19 +0100

    Zmiana nazwy

--- Test działania aplikacji/Properties/Resources.Designer.cs
+++ Test działania aplikacji/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Test_działania_aplikacji.Properties {
+namespace Paint_shop_app.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Test_działania_aplikacji.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Test_działania_aplikacji.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Paint_shop_app.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
