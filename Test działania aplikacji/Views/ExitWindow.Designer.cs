c2fcc3446a025a0111df250fb7ae5091a381afc2
Jakub Sulej <sulejmedia@gmail.com>
2020-05-03 12:57:07 +0200

    Reorganizing folders and files in project.

---
+++ Test działania aplikacji/Views/ExitWindow.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace PaintshopAppUI.Views
+{
+    partial class ExitWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExitWindow));
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.panelExitWindow = new System.Windows.Forms.Panel();
+            this.paneltop = new System.Windows.Forms.Panel();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelExitWindow.SuspendLayout();
+            this.paneltop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(180, 132);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(129, 48);
+            this.buttonExit.TabIndex = 0;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonYesExitWindow_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(26, 132);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(129, 48);
+            this.buttonCancel.TabIndex = 1;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancelExitWIndow_Click);
+            // 
+            // panelExitWindow
+            // 
+            this.panelExitWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.panelExitWindow.Controls.Add(this.label1);
+            this.panelExitWindow.Controls.Add(this.paneltop);
+            this.panelExitWindow.Controls.Add(this.buttonCancel);
+            this.panelExitWindow.Controls.Add(this.buttonExit);
+            this.panelExitWindow.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelExitWindow.Location = new System.Drawing.Point(0, 0);
+            this.panelExitWindow.Name = "panelExitWindow";
+            this.panelExitWindow.Size = new System.Drawing.Size(337, 214);
+            this.panelExitWindow.TabIndex = 2;
+            // 
+            // paneltop
+            // 
+            this.paneltop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.paneltop.Controls.Add(this.closeButton);
+            this.paneltop.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.paneltop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.paneltop.Location = new System.Drawing.Point(0, 0);
+            this.paneltop.Name = "paneltop";
+            this.paneltop.Size = new System.Drawing.Size(337, 31);
+            this.paneltop.TabIndex = 2;
+            this.paneltop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.paneltop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.paneltop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.closeButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.closeButton.FlatAppearance.BorderSize = 0;
+            this.closeButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.closeButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeButton.Image = ((System.Drawing.Image)(resources.GetObject("closeButton.Image")));
+            this.closeButton.Location = new System.Drawing.Point(312, 0);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(22, 31);
+            this.closeButton.TabIndex = 4;
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(95, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Are you sure you want to exit?";
+            // 
+            // ExitWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 213);
+            this.Controls.Add(this.panelExitWindow);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ExitWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ExitWindow";
+            this.panelExitWindow.ResumeLayout(false);
+            this.panelExitWindow.PerformLayout();
+            this.paneltop.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Panel panelExitWindow;
+        private System.Windows.Forms.Panel paneltop;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Test działania aplikacji/Views/JigViews/JigsSelector3.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using DataAccessLibrary.DataAccess;
+
+namespace PaintshopAppUI.Views.JigViews
+{
+    public partial class JigsSelector3 : Form
+    {
+        public JigsSelector3()
+        {
+            InitializeComponent();
+        }
+
+        int jigNumber = 3;
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            CloseThisPopupForm(null, null);
+        }
+
+        private void CloseThisPopupForm(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void JigsSelector3_Load(object sender, EventArgs e)
+        {
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.DeleteJigType(jigNumber);
+        }
+
+        private void buttonJig1_Click(object sender, EventArgs e)
+        {
+            string jigName = "F33DS1";
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.SetJigType(jigNumber, jigName);
+
+            CloseThisPopupForm(null, null);
+        }
+
+        private void buttonJig2_Click(object sender, EventArgs e)
+        {
+            string jigName = "F33DS2";
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.SetJigType(jigNumber, jigName);
+
+            CloseThisPopupForm(null, null);
+        }
+
+        private void buttonJig3_Click(object sender, EventArgs e)
+        {
+            string jigName = "VWFSI485";
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.SetJigType(jigNumber, jigName);
+
+            CloseThisPopupForm(null, null);
+        }
+
+        private void buttonJig4_Click(object sender, EventArgs e)
+        {
+            string jigName = "Targa";
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.SetJigType(jigNumber, jigName);
+
+            CloseThisPopupForm(null, null);
+        }
+
+        private void buttonJig5_Click(object sender, EventArgs e)
+        {
+            string jigName = "R231HD";
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.SetJigType(jigNumber, jigName);
+
+            CloseThisPopupForm(null, null);
+        }
+
+        private void buttonJig6_Click(object sender, EventArgs e)
+        {
+            string jigName = "VWTH";
+            JigsDataAccess db = new JigsDataAccess();
+
+            db.SetJigType(jigNumber, jigName);
+
+            CloseThisPopupForm(null, null);
+        }
+    }
+}
